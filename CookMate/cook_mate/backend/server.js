f026c9b22267f9e80e2c2d68ce36ea884a53907c
GeorgeGarry <leynovyegor@gmail.com>
2023-08-13 17:55:58 +0300

    app.listen(process.env.PORT || 3030, () => {

--- CookMate/cook_mate/backend/server.js
+++ CookMate/cook_mate/backend/server.js
@@ -126,7 +126,7 @@ app.get('/', (req, res) => {
 })
 
 // const port = 3030;
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT || 3030, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
 
